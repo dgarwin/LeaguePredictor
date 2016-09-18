5fd5895cb6cab4607fe6e7c3cfd9762e74d9f3c1
David Garwin <davidgarwin@gmail.com>
2016-09-18 01:19:28 -0400

    adds features and moves get_player_data to PlayerCollection; cleans up get_players; adds progress bar

--- PlayerCollection.py
+++ PlayerCollection.py
@@ -1,29 +1,45 @@
 import pandas as pd
-
+import numpy as np
 
 # TODO: Ignore items to ignore before processing
 
 
 class PlayerCollection():
+    ignore = ['item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'team']
+    categorical = ['playerPosition', 'playerRole']
+    distributions = {'CHALLENGER': 0.0002, 'MASTER': 0.0004, 'DIAMOND': 0.0183, 'PLATINUM': 0.0805, 'GOLD': 0.2351,
+                     'SILVER': 0.3897, 'BRONZE': 0.2759}
+
     # Setup
     def __init__(self, size=10000):
         self.size = size
         self.raw = {}
         self.division_counts = {'CHALLENGER': [], 'MASTER': [], 'DIAMOND': [], 'PLATINUM': [], 'GOLD': [], 'SILVER': [],
-                                'BRONZE': [], 'UNRANKED': []}  # init to avoid div by zero errors
+                                'BRONZE': []}  # init to avoid div by zero errors
         self.per_div_min = self.size / len(self.division_counts)
-        self.ignore = ['item0', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'team']
-        self.categorical = ['playerPosition', 'playerRole']
 
-    # Do we need more in this division?
+    def save(self):
+        np.save('players.npy', self.raw)
+        np.save('division_counts.npy', self.division_counts)
+
+    # Did we see this player already?
+    def has(self, player_id):
+        return player_id in self.raw
+
+    # Do we need this player?
     def need(self, player_id, division):
-        return player_id not in self.raw and \
+        return division in self.distributions and \
+               player_id not in self.raw and \
                len(self.division_counts[division]) < 1.0 * self.size / len(self.division_counts)
 
-    # Add new player to collection
-    def add(self, player_id, division, player_stats):
+    # Add new player to collection. Return next_ids for convenience.
+    def add(self, player_id, division, data):
+        if not self.need(player_id, division):
+            return []
+        player_stats, next_ids = data
         self.raw[player_id] = player_stats
         self.division_counts[division].append(player_id)
+        return next_ids
 
     # Get player ids from recent games
     @staticmethod
@@ -41,8 +57,21 @@ class PlayerCollection():
         games = pd.DataFrame([v['stats'] for v in recent_games['games'] if v['gameMode'] == 'CLASSIC'])
         if games.empty:
             return games, False
-        games = games.fillna(0)  # Fill before getting dummies
-        games = pd.get_dummies(games, columns=self.categorical)
+        games = games.fillna(0)  # Fill before getting
+        try:
+            games = pd.get_dummies(games, columns=self.categorical)
+        except ValueError:  # Ignore the column for now
+            pass
         # games = games.drop(self.ignore, axis=1)
         games = games.mean(axis=0)
         return games, True
+
+    def get_player_data(self, player_id, api):
+        if player_id in self.raw:
+            return {}, []
+        recent_games = api.recent_games(player_id)
+        player_stats, valid = self.get_player_stats(recent_games)
+        if not valid:  # No valid games found
+            return {}, []
+        next_ids = self.get_player_ids(recent_games)
+        return player_stats, next_ids
--- getgames.py
+++ getgames.py
@@ -1,17 +1,7 @@
 from collections import deque
 from PlayerCollection import PlayerCollection
 from LolApi import LolApi
-
-
-def get_player_data(player_id, players, api):
-    if player_id in players.raw:
-        return {}, []
-    recent_games = api.recent_games(player_id)
-    player_stats, valid = players.get_player_stats(recent_games)
-    if not valid:  # No valid games found
-        return {}, []
-    next_ids = players.get_player_ids(recent_games)
-    return player_stats, next_ids
+import progressbar
 
 
 # main logic loop
@@ -19,27 +9,35 @@ def get_players(seed_player_id, max_players=10000):
     players = PlayerCollection(max_players)
     queue = deque([seed_player_id])
     api = LolApi()
-    while not players.full() and len(queue) > 0:
-        buff = {}
-        while len(buff) < 10 and len(queue) > 0:
-            try:
-                player_id = queue.pop()
-                print str(1 + len(players.raw)) + ' ' + str(player_id)
-                player_stats, next_ids = get_player_data(player_id, players, api)
-                buff[player_id] = (player_stats, next_ids)
-            except Exception, e:
-                print 'Bad player: ' + str(player_id) + str(e)
-        solo_divisions = api.solo_divisions(buff.keys())
-        for player_id, data in buff.iteritems():
-            player_stats, next_ids = data
-            if player_id in solo_divisions:
+    buff = {}
+    last_progress = 0
+    with progressbar.ProgressBar(max_value=100) as progress:
+        # Progress bar
+        current_progress = 100 * len(players.raw) / max_players
+        if  current_progress > last_progress:
+            progress.update(current_progress)
+            players.save()
+        # Get at least max_players players
+        while not players.full() and len(queue) > 0:
+            for i in range(10):
+                if len(queue) == 0:
+                    break
+                try:
+                    player_id = queue.pop()
+                    if player_id not in buff and not players.has(player_id):
+                        buff[player_id] = players.get_player_data(player_id, api)
+                except Exception, e:
+                    print 'Bad player: ' + str(player_id) + str(e)
+            # Get solo divs from buffer
+            solo_divisions = api.solo_divisions(buff.keys())
+            # Add as needed
+            for player_id, data in buff.iteritems():
                 division = solo_divisions[player_id]
-                if players.need(player_id, division):
-                    players.add(player_id, division, player_stats)
-                    queue.extend(next_ids)
-        buff.clear()
-    return players
+                queue.extend(players.add(player_id, division, data))
+            # Refresh buffer
+            buff.clear()
+        return players
 
 
 if __name__ == '__main__':
-    print get_players(20886270, 10).raw
+    get_players(20649224, 10).raw
