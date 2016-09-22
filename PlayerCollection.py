2a561c4cf3f332f97e8636b2b2ad3c813a377ecd
David Garwin <davidgarwin@gmail.com>
2016-09-22 09:26:24 -0400

    fixes typo; removes .npy

--- PlayerCollection.py
+++ PlayerCollection.py
@@ -73,7 +73,7 @@ class PlayerCollection():
         games = games.mean(axis=0).to_dict()
         return games, True
 
-    def get_player_datagaa(self, player_id):
+    def get_player_data(self, player_id):
         if player_id in self.raw:
             return {}, []
         recent_games = self.api.recent_games(player_id)
