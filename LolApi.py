c935587d1bb2f7180f70b2fdcfa4f10529c10b74
David Garwin <davidgarwin@gmail.com>
2016-10-01 16:28:44 -0400

    fixes delay_time

--- LolApi.py
+++ LolApi.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime
 from collections import deque
 import time
-
+from datetime import timedelta
 
 class LolApi:
     def __init__(self, api_key='RGAPI-B110C515-1426-43EA-ABD8-F04D6169C9C1'):
@@ -65,20 +65,21 @@ class LolApi:
 
     @staticmethod
     def delay_time(queue, interval, requests, now):
-        while len(queue) > 0 and (now - queue[0]).total_seconds() > interval:
-            queue.popleft()
-        if len(queue) >= requests:
-            # wait a while
-            return (now - queue[0]).total_seconds()
-        else:
-            return 0
+        while len(queue) > 0 and len(queue) >= requests:
+            delta = (queue[0] + timedelta(0, interval) - now).total_seconds()
+            # First pop old off, then check if the queue is too long, then return
+            if delta < 0:
+                queue.popleft()
+            else:
+                return delta
+        return 0
 
     def request(self, url):
         # make a request within the rate limit. This could be done in a more async way, yolo
         now = datetime.now()
         short_delay = self.delay_time(self.short_req, 10, 10, now)
         long_delay = self.delay_time(self.long_req, 600, 500, now)
-        time.sleep(max([long_delay, short_delay]) * 1.1)
+        time.sleep(max([long_delay, short_delay]))
         for try_count in range(10):
             try:
                 request = Request(url)
